813f1043fa99639a63898ab69a69d65a32def9d9
abii16 <danielfekede1616@gmail.com>
2026-04-28 19:58:45 +0300

    Organize layout components into src/components/layout/ folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { AnimatePresence, motion } from 'framer-motion';
 import './App.css';
 
 // Components
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 // Pages
 import PrivacyPolicy from './pages/PrivacyPolicy';
